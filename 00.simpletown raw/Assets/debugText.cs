0e7d36c157c8300d098dd5ed0aff7745dfcc6d53
gareth-falmouth <gareth.lewis@falmouth.ac.uk>
2018-09-03 15:18:42 +0100

    .

---
+++ 00.simpletown raw/Assets/debugText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Find("bg").GetComponent<UnityEngine.UI.Text>().text = Text;
+        transform.Find("text").GetComponent<UnityEngine.UI.Text>().text = Text;
+    }
+
+    public String Text;
+}
---
+++ 00.simpletown raw/Assets/playercontroller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float movementForce = 1.0f;
+    public float cameraScale = 1;
+
+    bool trackMouse = true;
+
+    void Start () {
+		
+	}
+		
+	void Update ()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        var camera = transform.Find("Main Camera");
+
+        rigidBody.AddForce(camera.transform.forward * movementForce * Input.GetAxis("Vertical"), ForceMode.Impulse);
+        rigidBody.AddForce(camera.transform.right * movementForce * Input.GetAxis("Horizontal"), ForceMode.Impulse);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            trackMouse = !trackMouse;
+        }
+
+        var rotation = camera.localEulerAngles;
+          
+        rotation.y += Input.GetAxis("Mouse X") * cameraScale;
+        rotation.x -= Input.GetAxis("Mouse Y") * cameraScale;
+
+        if ((rotation.x > 30) && (rotation.x < 180))
+        {
+            rotation.x = 30;
+        }
+
+        if ((rotation.x < 330) && (rotation.x > 180))
+        {
+            rotation.x = 330;
+        }
+
+        GameObject.Find("debugtext").GetComponent<debugText>().Text = rotation.ToString();
+
+        if (trackMouse == true)
+        {
+            camera.localEulerAngles = rotation;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
